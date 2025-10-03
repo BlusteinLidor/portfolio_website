6c66178b4054f987d5242a433123b66960d466d2
Lidor Blustein <liidorr@gmail.com>
2025-10-03 13:43:44 +0300

    gitignore changes

--- youtubeShortsGenerator/try.js
+++
@@ -1,6 +0,0 @@
-// const res = fetch('view-source:https://www.youtube.com/@TechWithTim');
-// const html = res.body.innerHTML
-// console.log(html)
-
-fetch('https://api.codetabs.com/v1/proxy?quest=google.com').then((response) => response.text()).then((text) => console.log(text));
-
